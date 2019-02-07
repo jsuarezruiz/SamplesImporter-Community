350ed13191a6a52bc5ac71963d9a276dbd31a0f7
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2019-02-07 19:40:04 +0100

    Added Xamarin.NetFlix

---
+++ Xamarin.NetFlix/Xamarin.Netflix/Xamarin.Netflix/ViewModels/Base/Locator.cs
@@ -0,0 +1,55 @@
+﻿using Autofac;
+using System;
+using Xamarin.Netflix.Services.Movies;
+using Xamarin.Netflix.Services.Navigation;
+using Xamarin.Netflix.Services.Profile;
+using Xamarin.Netflix.Services.TVShows;
+using Xamarin.Netflix.Services.Watching;
+
+namespace Xamarin.Netflix.ViewModels.Base
+{
+    public class Locator
+    {
+        private readonly IContainer _container;
+        private ContainerBuilder _builder;
+
+        private static readonly Locator _instance = new Locator();
+
+        public static Locator Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        protected Locator()
+        {
+            _builder = new ContainerBuilder();
+
+            _builder.RegisterType<MoviesService>().As<IMoviesService>();
+            _builder.RegisterType<NavigationService>().As<INavigationService>();
+            _builder.RegisterType<ProfileService>().As<IProfileService>();
+            _builder.RegisterType<TVShowsService>().As<ITVShowsService>();
+            _builder.RegisterType<WatchingService>().As<IWatchingService>();
+
+            _builder.RegisterType<DetailViewModel>();
+            _builder.RegisterType<LoginViewModel>();
+            _builder.RegisterType<MenuViewModel>();
+            _builder.RegisterType<MainViewModel>();
+            _builder.RegisterType<HomeViewModel>();
+
+            _container = _builder.Build();
+        }
+
+        public T Resolve<T>()
+        {
+            return _container.Resolve<T>();
+        }
+
+        public object Resolve(Type type)
+        {
+            return _container.Resolve(type);
+        }
+    }
+}
